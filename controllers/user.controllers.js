951ac54336bce188a3beee057ce593dc1bd2b1e9
jokappo <kokerprim@gmail.com>
2024-11-21 23:08:46 +0000

    login/logout/token

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,63 +1,208 @@
 import sendEmail from "../config/senEmail.js";
 import UserModel from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import verifyEmailTemplate from "../utils/verifuEmailTemplate.js";
+import generatedAccessToken from "../utils/generatedAccessToken.js";
+import generateRefreshToken from "../utils/generateRefreshToken.js";
 
 export async function registerUserController(req, res) {
-    try {
-        const { name, email, password } = req.body
+  try {
+    const { name, email, password } = req.body;
 
-        if (!name || !email || !password) {
-            return res.status(400).json({
-                message: "provide name, email and password",
-                error : true,
-                success : false
-            })
-        }
-
-        const user = await UserModel.findOne({email})
-
-        if (user) {
-            return res.status(400).json({
-                message: "user already exists",
-                error : true,
-                success : false
-                })
-        }
-
-        const salt  = await bcryptjs.genSalt(10)
-        const hashedPassword = await bcryptjs.hash(password, salt)
-        const payload = {
-            name,
-            email,
-            password : hashedPassword
-        }
-        const newUser = new UserModel(payload)
-        const save = await newUser.save()
-
-        const verifyEmailUrl = `${process.env.FROMTEND_URL}/verify-email?code=${save?._id}`
-
-        const verify_email = await sendEmail({
-            sendTo : email,
-            subject : 'verify email from GraceShop',
-            html : verifyEmailTemplate({
-                name,
-                url : verifyEmailUrl
-            })
-        })
-
-        return res.json({
-            message : "user created successfully",
-            error : false,
-            success : true,
-            data : save
-        })
-
-    } catch (error) {
-        return res.status(500).json({
-             message: error.message || error,
-             error : true,
-             success : false
-        });
+    //verifier si l'utilisateur a rempli les cases
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "provide name, email and password",
+        error: true,
+        success: false,
+      });
     }
+    //verifier si l'email est valide
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "invalid email",
+        error: true,
+        success: false,
+      });
+    }
+    //verifier si l'email existe deja dans la base de données
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        message: "user already exists",
+        error: true,
+        success: false,
+      });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const payload = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+    const newUser = new UserModel(payload);
+    const save = await newUser.save();
+
+    const verifyEmailUrl = `${process.env.FROMTEND_URL}/verify-email?code=${save?._id}`;
+
+    const verify_email = await sendEmail({
+      sendTo: email,
+      subject: "verify email from GraceShop",
+      html: verifyEmailTemplate({
+        name,
+        url: verifyEmailUrl,
+      }),
+    });
+
+    return res.json({
+      message: "user created successfully",
+      error: false,
+      success: true,
+      data: save,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function verifyEmailController(req, res) {
+  try {
+    const { code } = req.body;
+    const user = await UserModel.findOne({ _id: code });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Invalide code",
+        error: true,
+        success: false,
+      });
+    }
+
+    const upateUser = await UserModel.updateOne(
+      { _id: code },
+      { verify_email: true }
+    );
+
+    return res.json({
+      message: "Email verified successfully",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+//login controller
+export async function loginController(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+        error: true,
+        success: false,
+      });
+    }
+
+    //verifier si l'utilisateur existe
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.status !== "active") {
+      return res.status(400).json({
+        message: "contact a admin",
+        error: true,
+        success: false,
+      });
+    }
+    //verifier le mot de passe
+    const isValidPassword = await bcryptjs.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(400).json({
+        message: "Invalid password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const accessToken = await generatedAccessToken(user._id);
+    const refreshToken = await generateRefreshToken(user._id);
+
+    const cookieOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    };
+    res.cookie("accessToken", accessToken, cookieOption);
+    res.cookie("refreshToken", refreshToken, cookieOption);
+
+    return res.json({
+      message: "Login success",
+      error: false,
+      success: true,
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+//lodout controller
+export async function logoutController(req, res) {
+  try {
+    const userId = req.userId
+
+
+    const cookieOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    };
+    res.clearCookie("accessToken", cookieOption);
+    res.clearCookie("refreshToken", cookieOption);
+
+    const removeRefreshToken = await UserModel.findByIdAndUpdate(userId, {
+      refresh_token : ""
+    })
+
+
+    return res.json({
+      message: "Logout success",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
 }
--- route/user.route.js
+++ route/user.route.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { registerUserController } from "../controllers/user.controllers.js";
+import { loginController, logoutController, registerUserController, verifyEmailController } from "../controllers/user.controllers.js";
+import auth from "../middleware/auth.js";
 
 const userRouter = Router()  // connection de l'utilisateur
 
-userRouter.post('/register',registerUserController)
+userRouter.post('/register',registerUserController),
+userRouter.post('/verify_email',verifyEmailController)
+userRouter.post('/login', loginController)
+userRouter.get('/logout', auth, logoutController)
+
 
 
 export default userRouter;
