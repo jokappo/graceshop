46603052a067a7f787d4bc4e4bbbc84bf0b43379
jokappo <kokerprim@gmail.com>
2025-01-17 16:07:56 +0000

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,26 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
   try {
-
     const token =
       req.cookies.accessToken || req?.header?.authorization?.split(" ")[1];
 
     if (!token) {
       return res.status(401).json({
-         message: "provide token" 
-        });
+        message: "provide token",
+      });
     }
 
-    const decode = await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN )
+    const decode = await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
     if (!decode) {
-        return res.status(401).json({
-            message : 'unauthorized access',
-            error : true,
-            success : false
-        })
+      return res.status(401).json({
+        message: "unauthorized access",
+        error: true,
+        success: false,
+      });
     }
-    req.userId = decode.id
-    next()
-
+    req.userId = decode.id;
+    next();
   } catch (error) {
     return res.status(500).json({
       message: error.message || error,
