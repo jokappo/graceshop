e57d2d336cd9d3e8e11df517239667c2dccea007
jokappo <kokerprim@gmail.com>
2025-02-26 13:15:12 +0000

    edit sub cat api router

--- route/subCategory.route.js
+++ route/subCategory.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { AddSubCategoryController, GetSubCategoryController } from "../controllers/subCategory.controller.js";
+import { AddSubCategoryController, GetSubCategoryController, UpdateSubCategoryController } from "../controllers/subCategory.controller.js";
 import auth from "../middleware/auth.js";
 
 const SubCategoryRouter = Router()
 
 SubCategoryRouter.post('/create', auth, AddSubCategoryController)
 SubCategoryRouter.get('/get', GetSubCategoryController)
+SubCategoryRouter.put('/update', auth, UpdateSubCategoryController)
 
 export default SubCategoryRouter
