11169da3f7d323b22f2f0ca8d11128f7b41a8d64
jokappo <kokerprim@gmail.com>
2025-02-27 09:18:43 +0000

    sub cat touter

--- route/subCategory.route.js
+++ route/subCategory.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AddSubCategoryController, GetSubCategoryController, UpdateSubCategoryController } from "../controllers/subCategory.controller.js";
+import { AddSubCategoryController, DeleteSubCategoryController, GetSubCategoryController, UpdateSubCategoryController } from "../controllers/subCategory.controller.js";
 import auth from "../middleware/auth.js";
 
 const SubCategoryRouter = Router()
@@ -7,5 +7,6 @@ const SubCategoryRouter = Router()
 SubCategoryRouter.post('/create', auth, AddSubCategoryController)
 SubCategoryRouter.get('/get', GetSubCategoryController)
 SubCategoryRouter.put('/update', auth, UpdateSubCategoryController)
+SubCategoryRouter.delete('/delete', auth, DeleteSubCategoryController)
 
 export default SubCategoryRouter
