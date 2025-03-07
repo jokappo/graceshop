3f242eaf2714f828ed5ec6182449ff67606eb10d
jokappo <kokerprim@gmail.com>
2025-03-07 22:13:23 +0000

    get product route

--- route/product.route.js
+++ route/product.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { AddProductContoller } from "../controllers/product.controller.js";
+import { AddProductContoller, GetProductController } from "../controllers/product.controller.js";
 
 const productRouter = Router();
 
 productRouter.post('/create', auth, AddProductContoller)
+productRouter.post('/get', GetProductController)
 
 export default productRouter
