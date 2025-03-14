69a2fe17381d734eaea70a23150de7f99d060acb
jokappo <kokerprim@gmail.com>
2025-03-14 23:35:16 +0000

    get by cat router

--- route/product.route.js
+++ route/product.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { AddProductContoller, GetProductController } from "../controllers/product.controller.js";
+import { AddProductContoller, GetProductByCategoryController, GetProductController } from "../controllers/product.controller.js";
 
 const productRouter = Router();
 
 productRouter.post('/create', auth, AddProductContoller)
 productRouter.post('/get', GetProductController)
+productRouter.post('/get-product-by-category', GetProductByCategoryController)
 
 export default productRouter
