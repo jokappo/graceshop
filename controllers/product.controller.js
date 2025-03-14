8f5062c62e37e4c2ad3e25d2f2ff3ec299862dd6
jokappo <kokerprim@gmail.com>
2025-03-14 23:34:55 +0000

    get by cat

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -34,34 +34,33 @@ export const AddProductContoller = async (req, res) => {
     }
 
     const product = new productModel({
-        name,
-        image,
-        category,
-        subCategory,
-        unit,
-        stock,
-        price,
-        dicount,
-        description,
-        more_details
-    })
+      name,
+      image,
+      category,
+      subCategory,
+      unit,
+      stock,
+      price,
+      dicount,
+      description,
+      more_details,
+    });
 
-    const save = await product.save()
+    const save = await product.save();
     if (!save) {
-        return res.status(404).json({
-            message: "Product not saved",
-            error: true,
-            success: false
-        })
+      return res.status(404).json({
+        message: "Product not saved",
+        error: true,
+        success: false,
+      });
     }
 
     return res.json({
-        message: "Product saved successfully",
-        error: false,
-        success: true,
-        data : save
-    })
-
+      message: "Product saved successfully",
+      error: false,
+      success: true,
+      data: save,
+    });
   } catch (error) {
     return res.status(500).json({
       message: error.message || error,
@@ -71,48 +70,77 @@ export const AddProductContoller = async (req, res) => {
   }
 };
 
-
 //get product
 export const GetProductController = async (req, res) => {
   try {
-
-    let { page, limit, search } = req.body
+    let { page, limit, search } = req.body;
     if (!page) {
-      page = 2
+      page = 2;
     }
 
     if (!limit) {
-      limit = 10
+      limit = 10;
     }
 
-    const query = search ? {
-      $text : {
-        $search : search
-      }
-    } : {}
-    const skip = (page - 1) * limit
+    const query = search
+      ? {
+          $text: {
+            $search: search,
+          },
+        }
+      : {};
+    const skip = (page - 1) * limit;
 
     const [data, totalCount] = await Promise.all([
-      productModel.find(query).sort({createdAt : -1}).skip(skip).limit(limit),
-      productModel.countDocuments(query)
-    ])
+      productModel.find(query).sort({ createdAt: -1 }).skip(skip).limit(limit),
+      productModel.countDocuments(query),
+    ]);
 
     return res.json({
       message: "Product retrieved successfully",
       error: false,
       success: true,
-      totalCount : totalCount,
-      totalNoPage : Math.ceil(
-        totalCount / limit
-      ),
-      data : data
-    })
+      totalCount: totalCount,
+      totalNoPage: Math.ceil(totalCount / limit),
+      data: data,
+    });
   } catch (error) {
     return res.status(500).json({
       message: error.message || error,
       success: false,
       error: true,
-    })
+    });
   }
-}
+};
 
+//get product by category
+export const GetProductByCategoryController = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if ( !id ) {
+      return res.status(400).json({
+        message: "Category id is required",
+        success: false,
+        error: true,
+      })
+    }
+
+    const data = await productModel
+      .find({ category: { $in : id } })
+      .sort({ createdAt: -1 })
+      .limit(15);
+
+      return res.json({
+        message: "Category product list",
+        error: false,
+        success: true,
+        data: data,
+      })
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      success: false,
+      error: true,
+    });
+  }
+};
