9d675844366c3b541cbef36d48784446a2b9460e
jokappo <kokerprim@gmail.com>
2025-03-24 23:04:24 +0000

    add route

--- route/product.route.js
+++ route/product.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { AddProductContoller, GetProductByCategoryController, GetProductController } from "../controllers/product.controller.js";
+import { AddProductContoller, GetProductByCategoryAndSubcategoryController, GetProductByCategoryController, GetProductController } from "../controllers/product.controller.js";
 
 const productRouter = Router();
 
 productRouter.post('/create', auth, AddProductContoller)
 productRouter.post('/get', GetProductController)
 productRouter.post('/get-product-by-category', GetProductByCategoryController)
+productRouter.post ('/get-product-by-category-and-subcategory', GetProductByCategoryAndSubcategoryController)
 
 export default productRouter
