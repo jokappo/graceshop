983e0e3995a24b4e7cd073c0d2bc41bd496c7cf7
jokappo <kokerprim@gmail.com>
2025-04-25 01:58:47 +0000

    route

--- route/product.route.js
+++ route/product.route.js
@@ -1,13 +1,36 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { AddProductContoller, GetProductByCategoryAndSubcategoryController, GetProductByCategoryController, GetProductController, GetProductDetailsController } from "../controllers/product.controller.js";
+import {
+  AddProductContoller,
+  DeleteProductControler,
+  GetProductByCategoryAndSubcategoryController,
+  GetProductByCategoryController,
+  GetProductController,
+  GetProductDetailsController,
+  UpdateProductController,
+} from "../controllers/product.controller.js";
+import { admin } from "../middleware/admin.js";
 
 const productRouter = Router();
 
-productRouter.post('/create', auth, AddProductContoller)
-productRouter.post('/get', GetProductController)
-productRouter.post('/get-product-by-category', GetProductByCategoryController)
-productRouter.post ('/get-product-by-category-and-subcategory', GetProductByCategoryAndSubcategoryController)
-productRouter.post ("/get-product-details",GetProductDetailsController)
+productRouter.post("/create", auth, admin, AddProductContoller);
+productRouter.post("/get", GetProductController);
+productRouter.post("/get-product-by-category", GetProductByCategoryController);
+productRouter.post(
+  "/get-product-by-category-and-subcategory",
+  GetProductByCategoryAndSubcategoryController
+);
+productRouter.post("/get-product-details", GetProductDetailsController);
 
-export default productRouter
+//upadate product
+productRouter.put(
+  "/update-product-details",
+  auth,
+  admin,
+  UpdateProductController
+);
+
+//delete
+productRouter.delete("/delete-product", auth, admin, DeleteProductControler);
+
+export default productRouter;
