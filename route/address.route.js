168640f602a46970b149eb8d7d7fe6481b4e7626
jokappo <kokerprim@gmail.com>
2025-05-16 20:28:45 +0000

    feat: add get address route with authentication

--- route/address.route.js
+++ route/address.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addAddressController } from "../controllers/address.controller.js";
+import { addAddressController, getAddressController } from "../controllers/address.controller.js";
 import auth from "../middleware/auth.js";
 
 const addressRouter = Router();
 
 addressRouter.post('/create', auth, addAddressController)
+addressRouter.get('/get', auth, getAddressController)
 
 
 export default addressRouter;
