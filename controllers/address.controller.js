f1379b38861d06a2a23a62395b6bccd935f2f131
jokappo <kokerprim@gmail.com>
2025-05-16 19:16:30 +0000

    feat: implement add address controller

---
+++ controllers/address.controller.js
@@ -0,0 +1,69 @@
+import addressModel from "../models/address.model.js";
+import UserModel from "../models/user.model.js";
+
+//add address controller
+export const addAddressController = async (req, res) => {
+  try {
+    const { address_line, city, state, pincode, country, mobile } = req.body;
+    const userId = req.userId;
+    console.log(userId);
+
+    // Check if all field are filled
+    if (!address_line || !city || !state || !pincode || !country || !mobile) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+        error: true,
+      });
+    }
+
+    const createAddress = new addressModel({
+      address_line,
+      city,
+      state,
+      pincode,
+      country,
+      mobile,
+      userId,
+    });
+
+    // Check if address already exists
+    const addressExists = await addressModel.findOne({
+      address_line,
+      city,
+      state,
+      pincode,
+      country,
+      mobile,
+    });
+
+    if (addressExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Address already exists",
+        error: true,
+      });
+    }
+    // Save address
+    const saveAddress = await createAddress.save();
+
+    const addUserAddress = await UserModel.findByIdAndUpdate(userId, {
+      $push: { address_details: saveAddress._id },
+    });
+
+    return res.json({
+      success: true,
+      error: false,
+      message: "Address added successfully",
+      data: saveAddress,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message || error,
+      error: true,
+    });
+  }
+};
+
+
