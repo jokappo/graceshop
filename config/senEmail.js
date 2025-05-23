df4c52d23336c76dbb4713f0a0d1e8f32ae510d8
jokappo <kokerprim@gmail.com>
2025-05-23 14:15:36 +0000

    fix: correct typo in sender name for email configuration

--- config/senEmail.js
+++ config/senEmail.js
@@ -11,7 +11,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({sendTo, subject, html}) => {
     try {
         const { data, error } = await resend.emails.send({
-            from: 'GraceSho <onboarding@resend.dev>',
+            from: 'GraceShop <onboarding@resend.dev>',
             to: sendTo,
             subject: subject,
             html: html,
