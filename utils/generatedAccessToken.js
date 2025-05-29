71046e642f8de382933db478d57352c30716c2dd
jokappo <kokerprim@gmail.com>
2025-05-29 23:21:51 +0000

    fix: update access token expiration time to 24 hours

--- utils/generatedAccessToken.js
+++ utils/generatedAccessToken.js
@@ -4,7 +4,7 @@ const generateAccessToken = async (userId) => {
   const token = await jwt.sign(
     { id: userId },
     process.env.SECRET_KEY_ACCESS_TOKEN,
-    { expiresIn: "5h" }
+    { expiresIn: "24h" }
   );
   return token;
 };
