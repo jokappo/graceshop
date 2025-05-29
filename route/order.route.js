0975d2f308fbdd14c36c60ce8975bf1dc59a562b
jokappo <kokerprim@gmail.com>
2025-05-29 23:24:36 +0000

    feat: add payment and webhook routes with authentication to order router

--- route/order.route.js
+++ route/order.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { cashOnDeliveryOrderController } from "../controllers/order.controller.js";
+import { cashOnDeliveryOrderController, paymentController, webhookStripeController } from "../controllers/order.controller.js";
+import auth from "../middleware/auth.js";
 
 const orderRouter = Router();
 
-orderRouter.post("/cash-on-delivery", cashOnDeliveryOrderController)
+orderRouter.post("/cash-on-delivery",auth, cashOnDeliveryOrderController)
+orderRouter.post("/checkout", auth, paymentController)
+orderRouter.post("/webhook", webhookStripeController)
+
 
 export default orderRouter;
